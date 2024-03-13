6a5196457973508144b8ee6209ded7397ecf4e5c
Andualem Assefa <andy2023user@gmail.com>
2024-03-13 07:06:47 -0700

    employeeAdded

---
+++ Api/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string MidelName{ get; set; }
+        public string LastName{ get; set; }
+        public string Departmnt { get; set; }
+    }
+}
